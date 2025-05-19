43a7909573a1d95c8ee26fb6d2746a6417dbdebf
hinata <hinatachikao1971@gmail.com>
2025-05-20 02:26:17 +0330

    some tiny change for project structure

--- src/header/testmodule.hpp
+++ src/header/testmodule.hpp
@@ -1,11 +1,14 @@
 #ifndef HEADER_TEST_MODULE_HPP
 #define HEADER_TEST_MODULE_HPP
 
+#pragma once
+
+#include "tm_header.hpp"
+
+
 /* declaration */
 namespace cpp {
-
-  
-
+    
 } // namespace cpp
 
 #endif /* HEADER_TEST_MODULE_HPP */
---
+++ src/header/tm_header.hpp
@@ -0,0 +1,8 @@
+#ifndef TM_HEADER_HPP
+#define TM_HEADER_HPP
+
+#pragma once
+
+
+
+#endif /* TM_HEADER_HPP */
