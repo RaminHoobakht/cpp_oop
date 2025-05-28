5609df907d5c8df1db035f9d630272e9665c7fb8
hinata <hinatachikao1971@gmail.com>
2025-05-28 08:28:56 +0330

    new cpp file

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,28 +5,10 @@
 
 #include "main.hpp"
 
-namespace one {
-    namespace two {
-        namespace three {
-            void print_message() noexcept;
-
-            [[maybe_unused]] void print_message() noexcept {
-                pout << "Using namespace aliases ..." << NL;
-            }
-        } // namespace three
-    } // namespace two
-} // namespace one
-
 
 int main() {
     /* code */
 
-    namespace np = one::two::three;
-
-    np::print_message();
-
-    pout << "\n #(07:32:57): The End ..." << eln;
+    pout << "\n #(08:27:51): The End ..." << eln;
     return EXIT_SUCCESS;
 }
-
-//(08:27:51)
